151e060d52fc32a41b0ad867d71f57ca24511b22
mrod502 <mrod22502@gmail.com>
2025-08-03 16:59:03 -0400

    chore: section implementation

--- bufr/src/message/section/mod.rs
+++ bufr/src/message/section/mod.rs
@@ -1,9 +1,24 @@
 mod section_0;
-
+mod section_1;
+mod section_5;
 pub use section_0::Section0;
+pub use section_1::Section1;
+pub use section_5::Section5;
 
-pub trait Section: Sized {
+pub trait Section
+where
+    Self: Sized,
+    Self::Error: Sized,
+{
     type Error;
     fn len(&self) -> usize;
     fn read(buf: &[u8]) -> Result<Self, Self::Error>;
+
+    fn validate_length(&self, buf: &[u8]) -> Result<(), ()> {
+        if self.len() > buf.len() {
+            Err(())
+        } else {
+            Ok(())
+        }
+    }
 }

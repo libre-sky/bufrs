d646beaa896540cb2d34463ccb57580ef75866c5
mrod502 <mrod22502@gmail.com>
2025-11-05 19:30:28 -0500

    chore(Message): struct def

--- bufr/src/message/mod.rs
+++ bufr/src/message/mod.rs
@@ -1,3 +1,16 @@
-pub mod section;
+use crate::message::section::{Section0, Section1, Section2, Section3, Section4, Section5};
 
-pub struct Message {}
+pub mod section;
+pub mod table;
+
+#[derive(Default, Debug)]
+pub struct Message {
+    s0: Section0,
+    s1: Section1,
+    s2: Option<Section2>,
+    s3: Section3,
+    s4: Section4,
+    s5: Section5,
+}
+
+impl Message {}
